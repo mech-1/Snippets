4a253e5c04ab28e6b77cf0e83c7f09a8286d6573
mech-1 <mech5579@gmail.com>
2025-06-29 23:13:49 +0200

    Add migration, modify templates, views and urls for snippets list and snippet detail.

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_page),
     path('snippets/add', views.add_snippet_page, name='add_snippet'),
     path('snippets/list', views.snippets_page, name='snippets_list'),
+    path('snippet/<int:id>', views.view_snippet, name='view_snippet'),
 ]
