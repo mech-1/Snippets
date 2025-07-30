36c70ee1f67050f942a37b567d65e49a18a11794
mech-1 <mech5579@gmail.com>
2025-07-30 10:25:11 +0200

    add test TestAddSnippetPage, rename tests.py to test_models.py, comment logging in settings.

---
+++ MainApp/test_views.py
@@ -0,0 +1,69 @@
+import pytest
+from django.contrib.auth.models import User, AnonymousUser
+
+from .views import add_snippet_page
+
+from django.test import Client, RequestFactory
+from django.urls import reverse
+from .models import Snippet
+
+class TestIndexPage:
+    def test_index_page(self):
+
+        client = Client()
+        response = client.get(reverse('home'))
+
+        assert response.status_code == 200
+        assert 'Добро пожаловать' in response.content.decode()
+        assert response.context.get('pagename') == 'PythonBin'
+
+@pytest.mark.django_db
+class TestAddSnippetPage:
+    def setup_method(self, method):
+        self.factory = RequestFactory()
+
+    def test_anonymous_user(self):
+        request = self.factory.get(reverse('snippet-add'))
+        request.user = AnonymousUser()
+        response = add_snippet_page(request)
+
+        assert response.status_code == 302
+
+    def test_authenticated_user(self):
+        request = self.factory.get(reverse('snippet-add'))
+        user = User.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="testpass12345",
+        )
+        request.user = user
+        response = add_snippet_page(request)
+
+        assert response.status_code == 200
+        # assert 'Добро пожаловать' in response.content.decode()
+        # assert response.context.get('pagename') == 'PythonBin'
+
+    def test_post_form_data(self):
+        form_data = {
+            "name": "Test form snippet",
+            "lang": "python",
+            "code": "print('Hello, world!')",
+            "public": True,
+        }
+        user = User.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="testpass12345",
+        )
+
+        request = self.factory.post(reverse('snippet-add'), form_data)
+        request.user = user
+        response = add_snippet_page(request)
+
+        snippet = Snippet.objects.get(id=1)
+
+        assert response.status_code == 302
+        assert snippet.name == form_data['name']
+        assert snippet.lang == form_data['lang']
+        assert snippet.code == form_data['code']
+        assert snippet.public == form_data['public']
